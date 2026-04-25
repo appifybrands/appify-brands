c69d1353b5efba24ef68817523d4c61e29bf5568
Nuh <mohammednuhabuhuseina@gmail.com>
2026-04-25 21:09:54 +0530

    removed layout

---
+++ src/app/real-estate/demo2/page.tsx
@@ -0,0 +1,7 @@
+export default function Demo2Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#0a0a0a] text-white">
+      <p>Coming Soon</p>
+    </div>
+  );
+}
